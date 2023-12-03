9f45e69d39faeb70185fa7e19259e8ee78096877
Hugo <hprietolopez@fp.mercedarias.es>
2023-12-03 11:15:03 +0100

    Update App.java

--- nuevoMaven/src/main/java/com/mgh/edd/App.java
+++ nuevoMaven/src/main/java/com/mgh/edd/App.java
@@ -8,6 +8,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World" );
+        System.out.println( "Maven:\r\n" + //
+                "\r\n" + //
+                "Utiliza XML para la configuración.\r\n" + //
+                "Enfoque \"opinado\" con convenciones predefinidas.\r\n" + //
+                "Ampliamente adoptado en la comunidad Java.\r\n" + //
+                "Basado en plugins y sigue un ciclo de vida predefinido.\r\n" + //
+                "Empaquetamiento convencional de proyectos.\r\n" + //
+                "Gradle:\r\n" + //
+                "\r\n" + //
+                "Utiliza un DSL basado en Groovy o Kotlin para la configuración.\r\n" + //
+                "Altamente flexible y permite una personalización extensa.\r\n" + //
+                "Rendimiento mejorado gracias a la construcción incremental.\r\n" + //
+                "Uso extendido en proyectos Android.\r\n" + //
+                "Integración sencilla con otros ecosistemas.\r\n" + //
+                "Ant:\r\n" + //
+                "\r\n" + //
+                "Utiliza archivos XML para la configuración.\r\n" + //
+                "Basado en tareas y metas.\r\n" + //
+                "Flexibilidad significativa pero menos estructura que Gradle.\r\n" + //
+                "Requiere más configuración manual.\r\n" + //
+                "Menos común en proyectos modernos, pero aún se utiliza." );
     }
 }
